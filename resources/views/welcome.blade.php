924a77734b4e09c9e808213472536dc1c1ad1979
Syy-syy-syy <yuya@artemis-tech.jp>
2020-04-21 01:58:31 +0000

    Message Model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    4月20日laravel開始
                 </div>
 
                 <div class="links">
