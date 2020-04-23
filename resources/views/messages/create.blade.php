22f907b8d67228fd7d2758cc05f7d1b2ec4d327e
Syy-syy-syy <yuya@artemis-tech.jp>
2020-04-23 08:24:18 +0000

    add column

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -1,17 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (count($errors) > 0)
-        <ul class="alert alert-danger" role="alert">
-            @foreach ($errors->all() as $error)
-                <li class="ml-4">{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
     <h1>メッセージ新規作成ページ</h1>
     <div class="row">
         <div class="col-6">
             {!! Form::model($message, ['route' => 'messages.store']) !!}
+                <div class="form-group">
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
                 <div class="form-group">
                     {!! Form::label('content', 'メッセージ:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
